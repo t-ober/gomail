19989fe9940de48532a68601f96133bdde695965
Thomas <thomas.oberliessen@googlemail.com>
2024-11-08 08:12:09 +0100

    Fix query test

--- service/query_test.go
+++ service/query_test.go
@@ -5,30 +5,30 @@ import "testing"
 func TestQuery(t *testing.T) {
 
 	testCases := []struct {
-		query gmailQuery
+		query GmailQuery
 		exp   string
 	}{
 		{
-			query: NewerThan(5, day),
+			query: NewerThan(5, Day),
 			exp:   "newer_than:5d",
 		},
 		{
-			query: NewerThan(5, day).And(From("Alice")),
+			query: NewerThan(5, Day).And(From("Alice")),
 			exp:   "(newer_than:5d) AND (from:Alice)",
 		},
 		{
-			query: (NewerThan(5, day).And(From("Alice")).Or(From("Bob"))),
+			query: (NewerThan(5, Day).And(From("Alice")).Or(From("Bob"))),
 			exp:   "((newer_than:5d) AND (from:Alice)) OR (from:Bob)",
 		},
 		{
-			query: NewerThan(5, day).And(From("Alice").Or(From("Bob"))),
+			query: NewerThan(5, Day).And(From("Alice").Or(From("Bob"))),
 			exp:   "(newer_than:5d) AND ((from:Alice) OR (from:Bob))",
 		},
 	}
 
 	for _, tc := range testCases {
-		if tc.query.query != tc.exp {
-			t.Fatalf("Expected %s, but got %s", tc.exp, tc.query.query)
+		if tc.query.Query != tc.exp {
+			t.Fatalf("Expected %s, but got %s", tc.exp, tc.query.Query)
 		}
 	}
 }
